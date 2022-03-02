eb8fdcf309fee5807a5aac74723ff9b17b6f8dce
Christian Sutter <git@doublesignal.com>
2022-03-02 18:04:59 +0000

    WIP

--- lib/classification/runner.rb
+++ lib/classification/runner.rb
@@ -1,15 +1,21 @@
 class Runner
-  attr_reader :examples, :actual, :prediction, :identifier, :fix
+  attr_reader :examples, :labels, :actual, :prediction, :identifier, :fix
 
-  def initialize(examples, actual:, prediction:, identifier:, fix: nil)
+  def initialize(examples, labels:, actual:, prediction:, identifier:, fix: nil)
     @examples = examples
+    @labels = labels
     @actual = actual
     @prediction = prediction
     @identifier = identifier
     @fix = fix
   end
 
-  def call # rubocop:disable Metrics/MethodLength
+  def call # rubocop:disable Metrics/MethodLength, Metrics/AbcSize, Metrics/PerceivedComplexity
+    true_positives = Hash.new(0)
+    false_positives = Hash.new(0)
+    true_negatives = Hash.new(0)
+    false_negatives = Hash.new(0)
+
     examples.each do |example|
       id = identifier.call(example)
       actual_value = actual.call(example)
@@ -17,9 +23,16 @@ class Runner
 
       if actual_value == predicted_value
         puts "✅ #{id} (#{actual_value})"
+
+        true_positives[actual_value] += 1
+        (labels - [actual_value]).each { |l| true_negatives[l] += 1 }
       else
         puts "🛑 #{id}"
         puts "  -> actual is '#{actual_value}', but predicted '#{predicted_value}'"
+
+        false_positives[predicted_value] += 1
+        false_negatives[actual_value] += 1
+        (labels - [actual_value, predicted_value]).each { |l| true_negatives[l] += 1 }
 
         if fix
           begin
@@ -32,6 +45,33 @@ class Runner
           end
         end
       end
+    end
+
+    accuracy = (true_positives.values.sum.to_f / examples.count).ceil(4)
+    puts "\nAccuracy: #{accuracy} over #{examples.count} examples"
+
+    precisions = labels
+      .select { |l| (true_positives[l] + false_positives[l]).positive? }
+      .to_h { |l| [l, true_positives[l].to_f / (true_positives[l] + false_positives[l])] }
+    puts "Precision per label:"
+    precisions.each do |label, precision|
+      puts "  #{label}: #{precision.ceil(4)}"
+    end
+
+    recalls = labels
+      .select { |l| (true_positives[l] + false_negatives[l]).positive? }
+      .to_h { |l| [l, true_positives[l].to_f / (true_positives[l] + false_negatives[l])] }
+    puts "Recall per label:"
+    recalls.each do |label, recall|
+      puts "  #{label}: #{recall.ceil(4)}"
+    end
+
+    specificities = labels
+      .select { |l| (true_negatives[l] + false_positives[l]).positive? }
+      .to_h { |l| [l, true_negatives[l].to_f / (true_negatives[l] + false_positives[l])] }
+    puts "Specificity per label:"
+    specificities.each do |label, specificity|
+      puts "  #{label}: #{specificity.ceil(4)}"
     end
   end
 end
--- lib/tasks/classify.rake
+++ lib/tasks/classify.rake
@@ -4,12 +4,13 @@ require_relative "../classification/main_job_role_classifier"
 namespace :classify do
   task main_job_role: :environment do
     Runner.new(
-      Vacancy.published.where("publish_on >= ?", "2022-02-24").limit(100).find_each,
+      Vacancy.published.where("publish_on >= ?", "2022-02-21").limit(50).find_each,
+      labels: %w[teacher teaching_assistant leadership education_support sendco other],
       actual: ->(ex) { ex.main_job_role },
       prediction: ->(ex) { MainJobRoleClassifier.new(ex.job_title).main_job_role },
       identifier: ->(ex) { ex.job_title },
       # Need to make sure not to reset main job value
-      fix: ->(ex, new_value) { ex.update(job_roles: (ex.job_roles - [ex.main_job_role]) + [new_value]) },
+      # fix: ->(ex, new_value) { ex.update(job_roles: (ex.job_roles - [ex.main_job_role]) + [new_value]) },
     ).call
   end
 end
