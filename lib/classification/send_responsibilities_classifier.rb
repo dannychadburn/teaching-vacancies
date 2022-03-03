813e021ee305998c7e85913f96f035a8dcaf9efa
Christian Sutter <git@doublesignal.com>
2022-03-03 15:23:04 +0000

    WIP - SEND done

--- lib/classification/send_responsibilities_classifier.rb
+++ lib/classification/send_responsibilities_classifier.rb
@@ -6,11 +6,10 @@ class SendResponsibilitiesClassifier
   end
 
   def naive
-    if vacancy.job_title.match?(/special|(learning support)|therapist|autism/i) ||
+    if vacancy.job_title.match?(/special\s|(learning support)|therapist|autism/i) ||
        vacancy.job_title.match?(/SEN|S\.E\.N|SEMH|LSA|LSSA/) ||
-       vacancy.salary.match?(/SEN/) ||
-       vacancy.job_advert.match?(/SEN|S\.E\.N/) ||
-       (vacancy.organisation && vacancy.organisation.gias_data["TypeOfEstablishment (name)"]&.match?(/special|(alternative provision)/i))
+       vacancy.salary.include?("SEN") ||
+       (vacancy.organisation && vacancy.organisation.gias_data["TypeOfEstablishment (name)"]&.match?(/special/i))
       "send_responsibilities"
     else
       "no_send_responsibilities"
