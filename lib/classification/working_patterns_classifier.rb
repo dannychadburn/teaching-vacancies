0dcdb3ab70dd5c59926c8e881fbccff00b5b41e1
Christian Sutter <git@doublesignal.com>
2022-03-03 16:05:44 +0000

    WIP - working patterns

--- lib/classification/working_patterns_classifier.rb
+++ lib/classification/working_patterns_classifier.rb
@@ -7,7 +7,8 @@ class WorkingPatternsClassifier
 
   def full_time
     if (vacancy.job_title.match?(/0\.\d|part[ -]?time|[^a-zA-Z]p\.?t[^\w]|breakfast|lunch|dinner|mid[ -]?day|hours|h\.?p\.?w/i) ||
-      vacancy.salary.match?(/rata|[^\d]0\.\d|part[ -]?time|[^a-zA-Z]p\.?t[^\w]/i)) &&
+      vacancy.salary.match?(/rata|[^\d]0\.\d|part[ -]?time|[^a-zA-Z]p\.?t[^\w]|[^a-zA-Z]p\.?h|(per hour)/i) ||
+      vacancy.job_advert.match?(/part[ -]?time/)) &&
        !vacancy.job_title.match?(/full[ -]?time/i)
       "not_full_time"
     else
